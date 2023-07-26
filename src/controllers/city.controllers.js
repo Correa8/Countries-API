84da1e868ce9201fb3cfc738689c063e3624b610
Daniela Correa <correadaniela783@gmail.com>
2023-07-26 16:37:00 -0500

    new-changes

--- src/controllers/city.controllers.js
+++ src/controllers/city.controllers.js
@@ -1,33 +1,33 @@
 const catchError = require('../utils/catchError');
-const Country = require('../models/Country');
+const City = require('../models/City');
 
 const getAll = catchError(async(req, res) => {
-    const results = await Country.findAll({ include: [City]});
+    const results = await City.findAll({ include : [Country]});
     return res.json(results);
 });
 
 const create = catchError(async(req, res) => {
-    const result = await Country.create(req.body);
+    const result = await City.create(req.body);
     return res.status(201).json(result);
 });
 
 const getOne = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await Country.findByPk(id);
+    const result = await City.findByPk(id);
     if(!result) return res.sendStatus(400);
     return res.json(result);
 });
 
 const remove = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await Country.destroy({ where: {id} });
+    const result = await City.destroy({ where: {id} });
     if(!result) return res.sendStatus(400);
     return res.sendStatus(204);
 });
 
 const update = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await Country.update(
+    const result = await City.update(
         req.body,
         { where: {id}, returning: true }
     );
